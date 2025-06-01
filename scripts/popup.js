67633c3a7c07cbcc79dd8c8a07770658049b26a9
Noel Joseph <shibuanuno@gmail.com>
2025-06-01 21:41:54 +0400

    change-1

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,5 +4,9 @@ const list_table = document.querySelector('.list_table');
 add_button.addEventListener("click",add_elements);
 
 function add_elements(){
-    list_table.innerHTML="TEST";
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        const currentUrl = currentTab.url;
+        list_table.textContent=currentUrl;
+    });
 };
