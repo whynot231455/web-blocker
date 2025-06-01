b75bbeb6773789a645ae0510dce6f991c3adf903
Noel Joseph <shibuanuno@gmail.com>
2025-06-02 01:24:30 +0400

    change-2

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,10 +3,37 @@ const list_table = document.querySelector('.list_table');
 
 add_button.addEventListener("click",add_elements);
 
-function add_elements(){
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const currentTab = tabs[0];
-        const currentUrl = currentTab.url;
-        list_table.textContent=currentUrl;
+async function getCurrentTabURL(){
+    const [tab] = await chrome.tabs.query({active: true , currentWindow: true});
+    return tab.url;
+}
+
+async function loadURL(){
+    const results = await chrome.storage.local.get('urls');
+    const urls = results.urls;
+    if (urls && Array.isArray(urls)){
+        urls.forEach(url => {
+            const li = document.createElement('li');
+            li.textContent = url;
+            list_table.appendChild(li);
+        });
+    }
+}
+
+async function add_elements(){
+    const currentTabURL= await getCurrentTabURL();
+    
+    //display URLS 
+    const li = document.createElement('li');
+    li.textContent=currentTabURL;
+    list_table.appendChild(li);
+
+    //saving URLS
+    chrome.storage.local.get('urls', (result) => {
+        const urls = result.urls || [];
+        urls.unshift(currentTabURL);
+        chrome.storage.local.set({ urls });
     });
-};
+}
+ 
+loadURL();
