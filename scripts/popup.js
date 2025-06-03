a5b9f4b6b4f135daba37c52c27f80852c00c460a
Noel Joseph <shibuanuno@gmail.com>
2025-06-03 15:11:31 +0400

    change-4

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,16 +1,25 @@
 const add_button = document.querySelector('.add_button');
 const list_table = document.querySelector('.list_table');
+const clear_button = document.querySelector('.clear_button');
 
 add_button.addEventListener("click",add_elements);
+clear_button.addEventListener("click",removeAll_elements);
+loadURL();
+getURLs();
 
 async function getCurrentTabURL(){
     const [tab] = await chrome.tabs.query({active: true , currentWindow: true});
     return tab.url;
 }
 
+async function getURLs(){
+    const results=await chrome.storage.local.get('urls');
+    const urls=results.urls;
+    return urls;
+}
+
 async function loadURL(){
-    const results = await chrome.storage.local.get('urls');
-    const urls = results.urls;
+    const urls=await getURLs();
     if (urls && Array.isArray(urls)){
         urls.forEach(url => {
             const li = document.createElement('li');
@@ -35,5 +44,11 @@ async function add_elements(){
         chrome.storage.local.set({ urls });
     });
 }
- 
-loadURL();
+
+async function removeAll_elements(){
+    await chrome.storage.local.set({ urls: [] });
+    const list_table=document.querySelector(".list_table");
+    if(list_table){
+        list_table.innerHTML="";
+    }
+}
